9919e0ddd79b7d1ffaff783eb68de8adc3076037
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-07-29 18:18:43 +0100

    working with python

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+best_club = input('Best football club in England?')
+
+trophies = input("How many trophies do you think they've won so far?")
+best_football = input("Who is the best footballer in the world?")
+
+
+madlibs = f"The best football club in England is {best_club}, they've won more than \
+{trophies} trophies so far. The best footballer in the world is {best_football}"
+
+print(madlibs)
